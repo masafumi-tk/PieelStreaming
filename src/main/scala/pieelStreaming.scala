ed0d0c16bc3b83eeb6c7061506a3b3c4802963b6
masafumi-tk <masafumi.cascata@gmail.com>
2016-04-01 19:15:32 +0900

    Sparkで感情解析

--- src/main/scala/pieelStreaming.scala
+++ src/main/scala/pieelStreaming.scala
@@ -2,8 +2,6 @@ import java.io.{InputStreamReader, FileInputStream}
 import java.util.Properties
 import java.util.regex.{Matcher, Pattern}
 import scala.collection.mutable.HashMap
-import scala.io.Source
-import java.io.FileReader;
 import kafka.producer.{KeyedMessage, Producer, ProducerConfig}
 import org.apache.spark.streaming.twitter.TwitterUtils
 import org.apache.spark.{SparkContext, SparkConf}
@@ -17,7 +15,7 @@ import org.atilika.kuromoji.{Token, Tokenizer}
  * ref:https://github.com/AKB428/inazuma/blob/master/src/main/scala/inazumaTwitter.scala
  * ref:http://www.intellilink.co.jp/article/column/bigdata-kk01.html
  */
-object pieelStreaming {
+object PieelStreaming {
   /**
    *
    * @param args args(0)=application.properties path (default config/application.properties)
@@ -25,7 +23,6 @@ object pieelStreaming {
   def main(args: Array[String]): Unit = {
     //--- Kafka Client Init End
     var configFileName: String = "config/application.properties"
-    println("test:"+FeelingDictionary.getFellingScore("今日はいい天気ですね"))
     if (args.length == 1) {
       configFileName = args(0)
     } 
@@ -59,14 +56,16 @@ object pieelStreaming {
     // debug
     // println(searchWordList(0))
 
-    //val stream = TwitterUtils.createStream(ssc, None, searchWordList)   //サーチしたい単語がある時
-    val stream = TwitterUtils.createStream(ssc, None) //ランダムに集計したい時
+    val stream = TwitterUtils.createStream(ssc, None, searchWordList)   //サーチしたい単語がある時
+    //val stream = TwitterUtils.createStream(ssc, None) //ランダムに集計したい時
+    var feelingScore : Integer  = null
 
     // Twitterから取得したツイートを処理する
     val tweetStream = stream.flatMap(status => {
 
       //val tokenizer : Tokenizer = CustomTwitterTokenizer.builder().build()  // kuromojiの分析器
       val features : scala.collection.mutable.ArrayBuffer[String] = new collection.mutable.ArrayBuffer[String]() //解析結果を保持するための入れ物
+      val featuresFeeling : scala.collection.mutable.HashMap[String,Double] = new collection.mutable.HashMap  [String,Double]() //解析結果を保持するための入れ物
       var tweetText : String = status.getText() //ツイート本文の取得
       val japanese_pattern : Pattern = Pattern.compile("[¥¥u3040-¥¥u309F]+") //「ひらがなが含まれているか？」の正規表現
       if(japanese_pattern.matcher(tweetText).find()) {  // ひらがなが含まれているツイートのみ処理
@@ -74,7 +73,7 @@ object pieelStreaming {
         tweetText = tweetText.replaceAll("http(s*)://(.*)/", "").replaceAll("¥¥uff57", "") // 全角の「ｗ」は邪魔www
 
         //感情解析
-        val feelingScore : Double = FeelingDictionary.getFellingScore(tweetText)
+
         // ツイート本文の解析
         val tokens : java.util.List[Token] = CustomTwitterTokenizer.tokenize(tweetText, dictFilePath)
         val pattern : Pattern = Pattern.compile("^[a-zA-Z]+$|^[0-9]+$") //「英数字か？」の正規表現
@@ -90,7 +89,7 @@ object pieelStreaming {
 
               println(tokens.get(index).getAllFeaturesArray()(1))
             } else if (tokens.get(index).getPartOfSpeech == "カスタム名詞") {
-              //println(tokens.get(index).getPartOfSpeech)
+              println(tokens.get(index).getPartOfSpeech)
               // println(tokens.get(index).getSurfaceForm)
               features += tokens.get(index).getSurfaceForm
             }
@@ -98,6 +97,7 @@ object pieelStreaming {
         }
       }
       (features)
+      //(featuresFeeling)
     })
 
     // ソート方法を定義（必ずソートする前に定義）
@@ -107,18 +107,24 @@ object pieelStreaming {
 
 
     // ウインドウ集計（行末の括弧の位置はコメントを入れるためです、気にしないで下さい。）
+
     val topCounts60 = tweetStream.map((_, 1)                      // 出現回数をカウントするために各単語に「1」を付与
     ).reduceByKeyAndWindow(_+_, Seconds(5*60)   // ウインドウ幅(60*60sec)に含まれる単語を集める
       ).map{case (topic, count) => (count, topic)  // 単語の出現回数を集計
     }.transform(_.sortByKey(true))               // ソート
 
-    val feelCount = 
+      /**
+    val topCountsFeeling = tweetStream.map((_, 1)                      // 出現回数をカウントするために各単語に「1」を付与
+    ).reduceByKeyAndWindow(_+_, Seconds(5*60)   // ウインドウ幅(60*60sec)に含まれる単語を集める
+    ).map{case (topic, count) => (count, topic)  // 単語の出現回数を集計
+    }.transform(_.sortByKey(true))               // ソート
+        */
     // TODO スコアリングはタイトルで1つに集計しなおす必要がある
     // TODO 俺ガイル, oregaisu => 正式タイトルに直して集計
 
-
     // 出力
     topCounts60.foreachRDD(rdd => {
+      //topCountsFeeling.foreachRDD(rdd => {
       // 出現回数上位20単語を取得
 
       // ソート
@@ -174,7 +180,7 @@ object FeelingDictionary{
 
   def setFeelMap(){
     var word : Array[String] = null
-    var source = scala.io.Source.fromFile("dictionary/FeelingScoreJP.txt")
+    val source = scala.io.Source.fromFile("dictionary/FeelingScoreJP.txt")
     source.getLines.foreach({line =>
       word = line.split(":",0)
       feelMap.put(word(0),word(3).toDouble+1)
